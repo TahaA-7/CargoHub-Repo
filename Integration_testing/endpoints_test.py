7cd25d51ac40f1259fd00d28ef178c0a0ba39c2e
wolfwlj <wolfmbo123@gmail.com>
2024-11-05 12:02:50 +0100

    warehouse integration tests

--- Integration_testing/endpoints_test.py
+++ Integration_testing/endpoints_test.py
@@ -44,11 +44,13 @@ def test_get_posted_warehouse():
     assert response.status_code == 200  # SUCCESS
 
 
+
+
 def test_put_warehouse():
     get_warehouse = requests.get(f"{BASE_URL}/warehouses/999999999", headers=HEADER)
     warehouse = get_warehouse.json()
     old_updated_at = warehouse['updated_at']
-    
+    # old_warehouse_code = 'YQZZNL56'
     warehouse['code'] = "YQZZNL56909090"
 
     response = requests.put(f"{BASE_URL}/warehouses/999999999", headers=HEADER, json=warehouse)
@@ -62,49 +64,51 @@ def test_put_warehouse():
     assert old_updated_at != new_updated_at  # SUCCESS
 
 
-# def test_get_updated_warehouse():
-#     local_updated_warehouse= {"id": 999999999,
-#                 "code": "YQZZNL56909090",
-#                 "name": "Heemskerk cargo hub",
-#                 "address": "Karlijndreef 281",
-#                 "zip": "4002 AS",
-#                 "city": "Heemskerk",
-#                 "province": "Friesland",
-#                 "country": "NL",
-#                 "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "1983-04-13 04:59:55", "updated_at": "2007-02-08 20:11:00"
-#                 }
-#     response = requests.get(f"{BASE_URL}/warehouses/{local_updated_warehouse['id']}", headers=HEADER)
-#     # check if the received data is the same as the data we posted
-#     assert response.json() == local_updated_warehouse
+def test_get_updated_warehouse():
+    local_updated_warehouse= {"id": 999999999,
+                "code": "YQZZNL56909090",
+                "name": "Heemskerk cargo hub",
+                "address": "Karlijndreef 281",
+                "zip": "4002 AS",
+                "city": "Heemskerk",
+                "province": "Friesland",
+                "country": "NL",
+                "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "1983-04-13 04:59:55", "updated_at": "2007-02-08 20:11:00"
+                }
+    response = requests.get(f"{BASE_URL}/warehouses/{local_updated_warehouse['id']}", headers=HEADER)
+    # check if the received data is the same as the data we posted
+    assert response.json()['code'] == local_updated_warehouse['code']  # SUCCESS
 
 
-# def test_delete_warehouse():
-#     local_warehouse= {"id": 999999999,
-#                 "code": "YQZZNL56909090",
-#                 "name": "Heemskerk cargo hub",
-#                 "address": "Karlijndreef 281",
-#                 "zip": "4002 AS",
-#                 "city": "Heemskerk",
-#                 "province": "Friesland",
-#                 "country": "NL",
-#                 "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "1983-04-13 04:59:55", "updated_at": "2007-02-08 20:11:00"
-#                 }
-#     response = requests.delete(f"{BASE_URL}/warehouses/{local_warehouse['id']}", headers=HEADER)
-#     assert response.status_code == 200  # SUCCESS
+def test_delete_warehouse():
+    local_warehouse= {"id": 999999999,
+                "code": "YQZZNL56909090",
+                "name": "Heemskerk cargo hub",
+                "address": "Karlijndreef 281",
+                "zip": "4002 AS",
+                "city": "Heemskerk",
+                "province": "Friesland",
+                "country": "NL",
+                "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "1983-04-13 04:59:55", "updated_at": "2007-02-08 20:11:00"
+                }
+    response = requests.delete(f"{BASE_URL}/warehouses/{local_warehouse['id']}", headers=HEADER)
+    assert response.status_code == 200  # SUCCESS
 
-# def test_get_deleted_warehouse():
-#     local_warehouse= {"id": 999999999,
-#                 "code": "YQZZNL56909090",
-#                 "name": "Heemskerk cargo hub",
-#                 "address": "Karlijndreef 281",
-#                 "zip": "4002 AS",
-#                 "city": "Heemskerk",
-#                 "province": "Friesland",
-#                 "country": "NL",
-#                 "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "1983-04-13 04:59:55", "updated_at": "2007-02-08 20:11:00"
-#                 }
-#     response = requests.get(f"{BASE_URL}/warehouses/{local_warehouse['id']}", headers=HEADER)
-#     assert response.status_code == 404  # NOT FOUND
+def test_get_deleted_warehouse():
+    local_warehouse= {"id": 999999999,
+                "code": "YQZZNL56909090",
+                "name": "Heemskerk cargo hub",
+                "address": "Karlijndreef 281",
+                "zip": "4002 AS",
+                "city": "Heemskerk",
+                "province": "Friesland",
+                "country": "NL",
+                "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "1983-04-13 04:59:55", "updated_at": "2007-02-08 20:11:00"
+                }
+    response = requests.get(f"{BASE_URL}/warehouses/{local_warehouse['id']}", headers=HEADER)
+
+    # print(response.json())
+    assert response.json() == None  # SUCCESS
 
 
 # def test_auth_get_warehouse():
