6c1020d24a2707f3d93abff4d28d971da64c29f6
choxkri <1059788@hr.nl>
2024-11-18 22:02:10 +0100

    Finished integration tests for item endpoints

--- Integration_testing/endpoints.md
+++ Integration_testing/endpoints.md
@@ -8,11 +8,11 @@ DELETE http://localhost:3000/api/v1/locations/{id} -> remove existing location
 Endpoints for items:
 GET http://localhost:3000/api/v1/items -> return all items ---Works
 GET http://localhost:3000/api/v1/items/{id} -> return specific item ---Works
-GET http://localhost:3000/api/v1/items/{id}/inventory -> return specific item inventories of locations ---Does not work
-GET http://localhost:3000/api/v1/items/{id}/inventory/totals -> return total stats of specific item ---Does not work
+GET http://localhost:3000/api/v1/items/{id}/inventory -> return specific item inventories of locations ---Does not work?
+GET http://localhost:3000/api/v1/items/{id}/inventory/totals -> return total stats of specific item ---Does not work?
 POST http://localhost:3000/api/v1/items -> add item ---Does not work
-PUT http://localhost:3000/api/v1/items/{id} -> update existing item 
-DELETE http://localhost:3000/api/v1/items/{id} -> remove existing item
+PUT http://localhost:3000/api/v1/items/{id} -> update existing item ---Works
+DELETE http://localhost:3000/api/v1/items/{id} -> remove existing item ---Works
 
 Endpoints for orders:
 GET http://localhost:3000/api/v1/orders -> return all orders
--- Integration_testing/test_items.py
+++ Integration_testing/test_items.py
@@ -52,77 +52,77 @@ def test_get_posted_item():
     response = requests.get(f"{BASE_URL}/items/{local_new_item['uid']}", headers=HEADER)
     # check if the received data is the same as the data we posted
 
-
-
-
-    
-
     assert response.json()["uid"] == local_new_item["uid"]
 
 
-# def test_put_warehouse():
-#     get_warehouse = requests.get(f"{BASE_URL}/warehouses/999999999", headers=HEADER)
-#     warehouse = get_warehouse.json()
-#     old_updated_at = warehouse['updated_at']
-#     # old_warehouse_code = 'YQZZNL56'
-#     warehouse['code'] = "YQZZNL56909090"
+def test_put_item():
+    get_item = requests.get(f"{BASE_URL}/items/P000001", headers=HEADER)
+    item = get_item.json()
+    old_updated_at = item['updated_at']
+    # old_warehouse_code = 'YQZZNL56'
+    item['code'] = "YQZZNL56909090"
 
-#     response = requests.put(f"{BASE_URL}/warehouses/999999999", headers=HEADER, json=warehouse)
+    response = requests.put(f"{BASE_URL}/items/{item['uid']}", headers=HEADER, json=item)
     
-#     assert response.status_code == 200  # SUCCESS
+    assert response.status_code == 200  # SUCCESS
 
-#     get_warehouse2 = requests.get(f"{BASE_URL}/warehouses/999999999", headers=HEADER)
-#     warehouse2 = get_warehouse2.json()
-#     new_updated_at = warehouse2['updated_at']
+    get_item2 = requests.get(f"{BASE_URL}/items/P000001", headers=HEADER)
+    item2 = get_item2.json()
+    new_updated_at = item2['updated_at']
 
-#     assert old_updated_at != new_updated_at  # SUCCESS
-
-
-# def test_get_updated_warehouse():
-#     local_updated_warehouse= {"id": 999999999,
-#                 "code": "YQZZNL56909090",
-#                 "name": "Heemskerk cargo hub",
-#                 "address": "Karlijndreef 281",
-#                 "zip": "4002 AS",
-#                 "city": "Heemskerk",
-#                 "province": "Friesland",
-#                 "country": "NL",
-#                 "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "1983-04-13 04:59:55", "updated_at": "2007-02-08 20:11:00"
-#                 }
-#     response = requests.get(f"{BASE_URL}/warehouses/{local_updated_warehouse['id']}", headers=HEADER)
-#     # check if the received data is the same as the data we posted
-#     assert response.json()['code'] == local_updated_warehouse['code']  # SUCCESS
+    assert old_updated_at != new_updated_at  # SUCCESS
 
 
-# def test_delete_warehouse():
-#     local_warehouse= {"id": 999999999,
-#                 "code": "YQZZNL56909090",
-#                 "name": "Heemskerk cargo hub",
-#                 "address": "Karlijndreef 281",
-#                 "zip": "4002 AS",
-#                 "city": "Heemskerk",
-#                 "province": "Friesland",
-#                 "country": "NL",
-#                 "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "1983-04-13 04:59:55", "updated_at": "2007-02-08 20:11:00"
-#                 }
-#     response = requests.delete(f"{BASE_URL}/warehouses/{local_warehouse['id']}", headers=HEADER)
-#     assert response.status_code == 200  # SUCCESS
 
-# def test_get_deleted_warehouse():
-#     local_warehouse= {"id": 999999999,
-#                 "code": "YQZZNL56909090",
-#                 "name": "Heemskerk cargo hub",
-#                 "address": "Karlijndreef 281",
-#                 "zip": "4002 AS",
-#                 "city": "Heemskerk",
-#                 "province": "Friesland",
-#                 "country": "NL",
-#                 "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "1983-04-13 04:59:55", "updated_at": "2007-02-08 20:11:00"
-#                 }
-#     response = requests.get(f"{BASE_URL}/warehouses/{local_warehouse['id']}", headers=HEADER)
 
-#     # print(response.json())
-#     assert response.json() == None  # SUCCESS
+def test_delete_warehouse():
+    local_new_item= {"uid": "P000001",
+        "code": "sjQ23408K",
+        "description": "Face-to-face clear-thinking complexity",
+        "short_description": "must",
+        "upc_code": "6523540947122",
+        "model_number": "63-OFFTq0T",
+        "commodity_code": "oTo304",
+        "item_line": 11,
+        "item_group": 73,
+        "item_type": 14,
+        "unit_purchase_quantity": 47,
+        "unit_order_quantity": 13,
+        "pack_order_quantity": 11,
+        "supplier_id": 34,
+        "supplier_code": "SUP423",
+        "supplier_part_number": "E-86805-uTM",
+        "created_at": "2015-02-19 16:08:24",
+        "updated_at": "2015-09-26 06:37:56"
+                }
+    response = requests.delete(f"{BASE_URL}/items/{local_new_item['uid']}", headers=HEADER)
+    assert response.status_code == 200  # SUCCESS
+    
+
+def test_get_deleted_warehouse():
+    local_new_item= {"uid": "P000001",
+        "code": "sjQ23408K",
+        "description": "Face-to-face clear-thinking complexity",
+        "short_description": "must",
+        "upc_code": "6523540947122",
+        "model_number": "63-OFFTq0T",
+        "commodity_code": "oTo304",
+        "item_line": 11,
+        "item_group": 73,
+        "item_type": 14,
+        "unit_purchase_quantity": 47,
+        "unit_order_quantity": 13,
+        "pack_order_quantity": 11,
+        "supplier_id": 34,
+        "supplier_code": "SUP423",
+        "supplier_part_number": "E-86805-uTM",
+        "created_at": "2015-02-19 16:08:24",
+        "updated_at": "2015-09-26 06:37:56"
+                }
+    response = requests.get(f"{BASE_URL}/items/P000001", headers=HEADER)
+
+    # print(response.json())
+    assert response.json() == None  # SUCCESS
 
 
 # def test_auth_get_warehouse():
