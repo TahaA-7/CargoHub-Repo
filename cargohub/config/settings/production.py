a894123ee02a52a0994cbaadea345b84b9dbb87c
wolfwlj <86714@glr.nl>
2025-01-21 00:48:37 +0100

    uashdaisd'as

--- cargohub/config/settings/production.py
+++ cargohub/config/settings/production.py
@@ -5,3 +5,22 @@ SECRET_KEY = os.getenv('DEPLOY_SSH_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': '/path/to/your/django_error.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
